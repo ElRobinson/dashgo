016181eebb52e9846a3efcec47c4b8fb08ab728d
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-09-05 18:00:32 -0300

    feat: added theme and some configs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS={true} theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
