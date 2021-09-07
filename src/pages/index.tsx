833d0c80a0bafa00abe824c0c34ce1989e36562e
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-09-07 16:07:14 -0300

    feat: created input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,21 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react"
+import { Flex, Button, Stack} from "@chakra-ui/react"
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
      <Flex as="form" width="100%" maxWidth={360} bg="gray.800" p="8" borderRadius={8} flexDirection="column">
        <Stack spacing="4">
-         <FormControl>
-          <FormLabel htmlFor="email">E-mail</FormLabel>
           <Input 
-              name="email" 
-              type="email" 
-              id="email"
-              focusBorderColor="pink.500" 
-              bgColor="gray.900" 
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="password">Password</FormLabel>
+            name="email"
+            type="email"
+            label="E-mail"
+          />
           <Input 
-              name="password" 
-              type="password"
-              id="password" 
-              focusBorderColor="pink.500" 
-              bgColor="gray.900" 
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-        </FormControl>
+            name="password"
+            type="password"
+            label="Senha"
+          />
         </Stack>
        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
      </Flex>
