b2cb16d9e6612697f1c3c43a7a137d441707ca63
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-09-12 17:13:28 -0300

    feat: add layout to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header (){
     return (
@@ -45,6 +45,36 @@ export function Header (){
                 
             </Flex>
 
+            <Flex 
+                align="center"
+                ml="auto"
+            >
+                <HStack 
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20"/>
+                    <Icon as={RiUserAddLine} fontSize="20"/>                
+                </HStack>
+
+                <Flex
+                    align="center"
+                >
+                    <Box
+                        mr="4"
+                        textAlign="right"
+                    >
+                        <Text>Luís</Text>
+                        <Text color="gray.300" fontSize="small">luisfelipe.robinson@gmail.com</Text>
+                    </Box>
+                    <Avatar size="md" name="luís felipe" src="https://avatars.githubusercontent.com/u/9387073?v=4"  />
+                </Flex>
+            </Flex>
         </Flex>
     );
 }
