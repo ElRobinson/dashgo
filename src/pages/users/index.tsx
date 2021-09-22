ef89505e0857db033169923d0f5b43d367288087
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-09-21 21:51:07 -0300

    feat: added pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox, Tbody, Td, Text } from '@chakra-ui/react'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
@@ -59,6 +60,9 @@ export default function UserList() {
                         </Tbody>
 
                     </Table>
+
+                    <Pagination/>
+
                 </Box>
             </Flex>
 
