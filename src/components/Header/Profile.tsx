9aafe432beefad5833105ceef34aa85087228e4b
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-09-26 20:48:28 -0300

    feat: changed header components to be responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,24 @@
 import { Flex, Text, Box, Avatar } from '@chakra-ui/react'
 
-export function Profile(){
+interface ProfileProps {
+    showProfileData?: boolean, 
+}
+
+export function Profile({showProfileData = true}:ProfileProps){
     return (
         <Flex
             align="center"
         >
-            <Box
-                mr="4"
-                textAlign="right"
-            >
-                <Text>Luís</Text>
-                <Text color="gray.300" fontSize="small">luisfelipe.robinson@gmail.com</Text>
-            </Box>
+            { showProfileData && (
+                <Box
+                    mr="4"
+                    textAlign="right"
+                >
+                    <Text>Luís</Text>
+                    <Text color="gray.300" fontSize="small">luisfelipe.robinson@gmail.com</Text>
+                </Box>
+            )}
+            
             <Avatar size="md" name="luís felipe" src="https://avatars.githubusercontent.com/u/9387073?v=4"  />
         </Flex>
     );
