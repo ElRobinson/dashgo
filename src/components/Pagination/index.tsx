2648916eeb9f39da9a3c6da728ba54629edfac16
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-09-26 20:21:31 -0300

    refactor: sorting out components

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,26 @@
+import { Stack, Button, Box } from "@chakra-ui/react";
+import { PaginationItem } from "./PaginationItem";
+
+export function Pagination(){
+    return (
+        <Stack
+            direction="row"
+            mt="8"
+            justify="space-between"
+            align="center"
+            spacing="6"
+        >
+            <Box>
+                <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+            </Box>
+           <Stack
+            direction="row"
+            spacing="2"
+           >
+               <PaginationItem isCurrent number={1}/>
+               <PaginationItem number={2}/>     
+               <PaginationItem number={3}/>
+           </Stack>
+        </Stack>
+    );
+}
---
+++ src/components/Sidebar/NavSection.tsx
@@ -0,0 +1,19 @@
+import { Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import { RiContactsLine, RiDashboardLine } from "react-icons/ri";
+
+interface NavPropsSection {
+    title: string;
+    children: ReactNode;
+}
+
+export function NavSection({ title, children } : NavPropsSection) {
+    return (
+        <Box>
+            <Text fontWeight="bold" color="gray.400" fontSize="small">{title}</Text>
+            <Stack spacing="4" mt="8" align="stretch">
+                {children}               
+            </Stack>
+        </Box>
+    );
+}
