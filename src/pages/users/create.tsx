68ab76a2993e0289a53865abde0aad9361b271e0
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-10-10 21:42:57 -0300

    feat: added navegation flux

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -4,6 +4,8 @@ import { Input } from '../../components/Form/Input';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 
+import Link from 'next/link';
+
 export default function CreateUser() {
     return (
         <Box>
@@ -31,7 +33,10 @@ export default function CreateUser() {
 
                   <Flex mt="8" justify="flex-end">
                       <HStack spacing="4">
-                        <Button colorScheme="whiteAlpha">Cancelar</Button>
+                        <Link href="/users" passHref>
+                            <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+                        </Link>
+                        
                         <Button colorScheme="pink">Salvar</Button>
                       </HStack>
                   </Flex>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -3,6 +3,7 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import Link from 'next/link';
 
 export default function UserList() {
     const isWideVersion = useBreakpointValue({
@@ -23,9 +24,11 @@ export default function UserList() {
                             Usuários
                         </Heading>
 
-                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>} >
-                            Criar novo
-                        </Button>
+                        <Link href="users/create" passHref>
+                            <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} fontSize="20"/>} >
+                                Criar novo
+                            </Button>
+                        </Link>
 
                     </Flex>
 
