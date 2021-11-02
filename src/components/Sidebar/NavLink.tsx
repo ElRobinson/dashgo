b721ca5af71c946317b7036fb201e541cc872d31
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2021-11-02 14:01:08 -0300

    Feat: make linkActive

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,7 @@
 import { Text, Link as ChakraLink, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
-import Link from 'next/link';
+
+import { ActiveLink } from "../ActiveLink";
 
 interface NavLinksProps extends ChakraLinkProps {
     icon: ElementType;
@@ -10,11 +11,11 @@ interface NavLinksProps extends ChakraLinkProps {
 
 export function NavLink( {icon, children, href, ...rest}: NavLinksProps){
     return(
-        <Link href={href} passHref >        
+        <ActiveLink href={href} passHref >        
             <ChakraLink display="flex" align="center" {...rest}> 
                 <Icon as={icon} fontSize="20"/>
                 <Text ml="4" fontWeight="medium"> {children}</Text>
             </ChakraLink>
-        </Link>
+        </ActiveLink>
     );
 }
