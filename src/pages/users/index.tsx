98881d9e4a9c290caee72159410cf2ad7800e4a3
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2022-01-09 22:13:38 -0300

    Feat: refetch on user pahe

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -8,7 +8,7 @@ import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 
 export default function UserList() {
-    const { data, isLoading, error} = useQuery('users', async () => {
+    const { data, isLoading, isFetching, error} = useQuery('users', async () => {
        const response = await fetch('http://localhost:3000/api/users')
        const data = await response.json()
 
@@ -44,6 +44,8 @@ export default function UserList() {
                     <Flex mb="8" justify="space-between" align="center">
                         <Heading size="lg" fontWeight="normal">
                             Usuários
+
+                            { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
                         </Heading>
 
                         <Link href="users/create" passHref>
