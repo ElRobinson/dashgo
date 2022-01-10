76764ad60fc04e32123fd36d801253906423fc55
Luis Felipe Trein Robinson <luisfelipe.robinson@gmail.com>
2022-01-09 22:19:48 -0300

    feat: added axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,11 +6,11 @@ import { Sidebar } from '../../components/Sidebar';
 import Link from 'next/link';
 import { useEffect } from 'react';
 import { useQuery } from 'react-query';
+import { api } from '../../services/api';
 
 export default function UserList() {
-    const { data, isLoading, isFetching, error} = useQuery('users', async () => {
-       const response = await fetch('http://localhost:3000/api/users')
-       const data = await response.json()
+    const { data, isLoading, isFetching, error} = useQuery('users', async () => {       
+       const { data } = await api.get('users')
 
        const users = data.users.map(user => {
            return {
